4933e4340b91a7e9096faba282eb7fe826778a7a
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-10-21 21:40:41 +0200

    adding card theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,23 +1,27 @@
-
 import 'package:ecommerce/presentation/color_manager.dart';
+import 'package:ecommerce/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1
-      // cardview theme
+    // main colors
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    // cardview theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
-      // app bar theme
+    // app bar theme
 
-      // button theme
+    // button theme
 
     // text theme
 
-      // input decoration theme (text form field)
-
-      );
+    // input decoration theme (text form field)
+  );
 }
